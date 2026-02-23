0b0b4208a7bf6ae873b998c547ef526631c836f8
moorthydharun14-dot <moorthydharun14@gmail.com>
2026-02-23 22:08:22 +0530

    Delete Solving System of linear equations.py

--- Solving System of linear equations.py
+++
@@ -1,8 +0,0 @@
-#Program to find the solution for the given linear equations.
-#Developed by: DHARUN M 25018453
-#RegisterNumber:212225230057
-import numpy as np
-a = np.array([[1,3],[2,5]])
-b = np.array([5,-3])
-x = np.linalg.solve(a,b)
-print(x)
